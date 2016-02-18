8997c5877e6da08145b247f1660b8476d0235688
Marty Kowbel <mekobes44@yahoo.com>
2016-02-18 18:53:23 -0500

    recipe box in progress

---
+++ recipe_box/app/models/recipe.rb
@@ -0,0 +1,4 @@
+class Recipe < ActiveRecord::Base
+  has_attached_file :avatar, styles: { medium: '300x300#' }, default_url: '/images/:style/missing.png'
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+end
