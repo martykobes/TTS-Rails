fc02412cf5bd222530e8c073d3a994c6966b4347
Marty Kowbel <mekobes44@yahoo.com>
2016-02-19 11:08:44 -0500

    added long and lat

--- travel/db/schema.rb
+++ travel/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160205140051) do
+ActiveRecord::Schema.define(version: 20160219153418) do
 
   create_table "destinations", force: :cascade do |t|
     t.string   "city"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 20160205140051) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "address"
   end
 
   create_table "users", force: :cascade do |t|
